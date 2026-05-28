b858e006c16843839d018c567f977f4a6ce07efe
albertomerino3 <albertocampoymerino@gmail.com>
2026-05-28 17:54:08 +0200

    añador bienvenida y creación de jugadores

--- juegoOca2/src/main/java/org/JuegoOca/JuegoOcaMain.java
+++ juegoOca2/src/main/java/org/JuegoOca/JuegoOcaMain.java
@@ -5,19 +5,54 @@
  */
 package org.JuegoOca;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class JuegoOcaMain {
 
     // MAIN
     public static void main(String[] args) {
 
-        // Creo tableroooo
+        // Creamos el tablero que carga el archivo JSON con las casillas
         Tablero tablero = new Tablero();
 
         // muestro info del juegorrr
-        System.out.println("=== JUEGO DE LA OCA ===");
-        System.out.println(tablero);
+        System.out.println("==============================================");
+        System.out.println("BIENVENIDO / BIENVENIDA AL JUEGO DE LA OCA");
+        System.out.println("Una aventura de azar, peligros y estrategia");
+        System.out.println("Hecha por Alberto, Manuel y Nacho :)");
+        System.out.println("==============================================");
         System.out.println();
+
+        //creamos el scanner para escribir
+        Scanner teclado = new Scanner(System.in);
+
+        //creamos una lista donde se guardarán los jugadores
+        ArrayList<Jugador> listaJugadores = new ArrayList<>();
+
+        System.out.println("El tablero se ha creado correctamente, con " + tablero.getNumCasillas() + " casillas en total.");
+        System.out.println("Esperando a los jugadores ... ...");
+
+        
+        //PARTE DE CREACIÓN DE JUGADORES
+
+        //preguntamos número de jugadores
+        System.out.println("¿Cuántos participantes van a jugar?");
+        int numParticipantes = teclado.nextInt();
+        teclado.nextLine(); // limpiar el salto de línea
+
+        //bucle que se repite según jugadores
+        for (int i = 1; i <= numParticipantes; i++) {
+            System.out.println("Introduce el nombre del jugador " + i + ":");
+            String nombreJugador = teclado.nextLine();
+            Jugador jugador = new Jugador(nombreJugador);
+            listaJugadores.add(jugador);
+        } 
+
+        System.out.println("Los jugadores están listos... ... ...");
+
+
 
         // se recorren todas las casillas del tablero
         for (int i = 1; i <= tablero.getNumCasillas(); i++) {
